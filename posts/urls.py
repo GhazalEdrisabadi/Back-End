cba66ccfdc7ad95e603ae96682c4268c56c39d51
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-06 09:50:26 +0430

    Add url of post delete.

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,7 @@ from .views import (
     post_create,
     post_detail,
     post_update,
+    post_delete,
 
 )
 
@@ -14,4 +15,5 @@ urlpatterns = [
     url(r'^create/$', post_create),
     url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
     url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
 ]
