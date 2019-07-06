8cf2e727b36670c1af00bfd1142371526f651f0f
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-06 10:55:19 +0430

    Add PostCreateApi url to Create post.

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -3,10 +3,9 @@ from django.contrib import admin
 
 from .views import (
     PostListAPIView,
-
-)
+    PostCreateAPIView)
 
 urlpatterns = [
     url(r'^$', PostListAPIView.as_view(), name='list'),
-
+    url(r'^create/$', PostCreateAPIView.as_view(), name='create'),
 ]
