efedb4406410e477aaab076f895f4e0a0601810d
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-06 10:34:09 +0430

    Add post detail class to access detail serializer.

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,12 +1,14 @@
 from rest_framework.generics import (
     CreateAPIView,
-)
+    RetrieveAPIView)
+from rest_framework.permissions import AllowAny
+
 from posts.models import Post
 
 from .serializers import (
     PostCreateUpdateSerializer,
 
-)
+    PostDetailSerializer)
 
 
 class PostCreateAPIView(CreateAPIView):
@@ -15,3 +17,10 @@ class PostCreateAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
+    permission_classes = [AllowAny]
