5f6b610c55c42775a14aaa94cd1fd00d64b1638a
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-06 10:57:34 +0430

    Add PostDeleteApiView to Delete Posts

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 
 from .views import (
     PostListAPIView,
-    PostCreateAPIView, PostDetailAPIView)
+    PostCreateAPIView, PostDetailAPIView, PostDeleteAPIView)
 
 urlpatterns = [
     url(r'^$', PostListAPIView.as_view(), name='list'),
     url(r'^create/$', PostCreateAPIView.as_view(), name='create'),
     url(r'^(?P<slug>[\w-]+)/$', PostDetailAPIView.as_view(), name='detail'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', PostDeleteAPIView.as_view(), name='delete'),
 ]
