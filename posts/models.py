558acf5e2259cd83e2bba7ee3d048e7439f74d32
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-06 10:05:08 +0430

    Add pre_save function to have access of draft and pre_save.

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 from django.utils.text import slugify
 from .utils import get_read_time
+from django.db.models.signals import pre_save
 
 
 
@@ -81,3 +82,16 @@ def create_slug(instance, new_slug=None):
         new_slug = "%s-%s" % (slug, qs.first().id)
         return create_slug(instance, new_slug=new_slug)
     return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+    if instance.content:
+        html_string = instance.get_markdown()
+        read_time_var = get_read_time(html_string)
+        instance.read_time = read_time_var
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
