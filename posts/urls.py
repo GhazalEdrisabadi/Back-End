5ea0450bda471875a3ae57e247a5f2e78da61007
Mobina Kashanian <Mobina.km@gmail.com>
2019-07-06 09:48:06 +0430

    Add url of post create.

--- posts/urls.py
+++ posts/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 
 from .views import (
     post_list,
+    post_create,
 
 )
 
 urlpatterns = [
     url(r'^$', post_list, name='list'),
-
+    url(r'^create/$', post_create),
 ]
